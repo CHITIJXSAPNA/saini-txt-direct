d7267eec59ba78ceecad3129f50dc53e5a8c6f2d
CHITIJXSAPNA <parbhatkumarkori@gmail.com>
2025-06-14 10:01:37 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,12 +3,12 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "xxxxxxxxx"))
-API_HASH = environ.get("API_HASH", "xxxxxxxxxxxxxxxxxxxxxxx")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "5680454765"))
-CREDIT = "𝄟⃝‌🐬🇳‌ɪᴋʜɪʟ𝄟⃝🐬"
-AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
+API_ID = int(environ.get("API_ID", "28712726"))
+API_HASH = environ.get("API_HASH", "06acfd441f9c3402ccdb1945e8e2a93b")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7894993684:AAGossQUEP_OknJfPQvAJLPUHOrUemFxX1g")
+OWNER = int(environ.get("OWNER", "1003575883"))
+CREDIT = "CHITIJRAJPUT"
+AUTH_USER = os.environ.get('AUTH_USERS', '1003575883').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
